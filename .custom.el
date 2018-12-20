760e57db7a488aef56c03c55e2b5a3ffbb19cc2d
Nick <nick@x230>
2018-12-20 23:41:11 +0200

    git gutter, etc.

--- .custom.el
+++ .custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (google-this magit yasnippet-snippets xah-math-input which-key visual-fill-column use-package try telephone-line smex smartparens shackle projectile popwin pdf-tools org-bullets olivetti latex-unicode-math-mode latex-pretty-symbols hydra helpful helm-flycheck helm-describe-modes helm-descbinds helm-company flycheck-pos-tip expand-region evil-surround evil-nerd-commenter evil-leader evil-goggles evil-commentary evil-collection company-math company-c-headers avy adaptive-wrap ace-jump-mode))))
+    (git-gutter discover-my-major yasnippet-snippets xah-math-input which-key visual-fill-column use-package try telephone-line smex smartparens shackle projectile popwin pdf-tools org-bullets olivetti magit latex-unicode-math-mode latex-pretty-symbols hydra helpful helm-flycheck helm-describe-modes helm-descbinds helm-company google-this flycheck-pos-tip expand-region evil-surround evil-nerd-commenter evil-leader evil-goggles evil-commentary evil-collection company-math company-c-headers avy adaptive-wrap ace-jump-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
