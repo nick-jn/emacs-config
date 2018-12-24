f8e15d604818e948437630242f5e392f0735f888
Nick <nick@x230>
2018-12-24 15:32:00 +0200

    ibuffer fix, etc

--- .custom.el
+++ .custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (highlight-indent-guides yasnippet-snippets xah-math-input which-key visual-fill-column use-package try telephone-line spaceline smooth-scrolling smex smartparens smart-mode-line shackle projectile popwin pdf-tools org-evil org-bullets olivetti latex-unicode-math-mode latex-pretty-symbols hydra helpful helm-flycheck helm-describe-modes helm-descbinds helm-company google-this git-gutter flycheck-pos-tip feebleline expand-region evil-surround evil-org evil-nerd-commenter evil-magit evil-leader evil-goggles evil-commentary evil-collection doom-modeline discover-my-major company-math company-c-headers avy adaptive-wrap ace-jump-mode))))
+    (treemacs-projectile treemacs-evil treemacs yasnippet-snippets xah-math-input which-key visual-fill-column use-package try telephone-line spaceline smooth-scrolling smex smartparens smart-mode-line shackle popwin pdf-tools org-evil org-bullets olivetti latex-unicode-math-mode latex-pretty-symbols ibuffer-projectile hydra highlight-indent-guides helpful helm-projectile helm-flycheck helm-describe-modes helm-descbinds helm-company google-this git-gutter flycheck-pos-tip feebleline expand-region evil-surround evil-org evil-nerd-commenter evil-magit evil-leader evil-goggles evil-commentary evil-collection doom-modeline discover-my-major company-math company-c-headers avy adaptive-wrap ace-jump-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
