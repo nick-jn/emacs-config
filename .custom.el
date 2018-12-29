fcb1d6d105180eb537f839130c08e61fa852bd88
Nick <nick@x230>
2018-12-29 20:51:44 +0200

    Minor changes

--- .custom.el
+++ .custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (dumb-jump helm-ag yasnippet-snippets xah-math-input which-key visual-fill-column use-package try treemacs-projectile treemacs-evil telephone-line spaceline smooth-scrolling smex smartparens smart-mode-line shackle rainbow-delimiters popwin pdf-tools paradox org-evil org-bullets olivetti latex-unicode-math-mode latex-pretty-symbols ibuffer-projectile highlight-indent-guides helpful helm-projectile helm-flycheck helm-describe-modes helm-descbinds helm-company google-this git-gutter flycheck-pos-tip feebleline expand-region evil-surround evil-org evil-nerd-commenter evil-magit evil-leader evil-goggles evil-commentary evil-collection doom-modeline discover-my-major company-math company-c-headers adaptive-wrap ace-jump-mode))))
+    (centered-cursor-mode yasnippet-snippets xah-math-input which-key visual-fill-column use-package try treemacs-projectile treemacs-evil telephone-line spaceline smooth-scrolling smex smartparens smart-mode-line shackle rainbow-delimiters popwin pdf-tools paradox org-evil org-bullets olivetti latex-unicode-math-mode latex-pretty-symbols ibuffer-projectile highlight-indent-guides helpful helm-projectile helm-flycheck helm-describe-modes helm-descbinds helm-company helm-ag google-this git-gutter flycheck-pos-tip flycheck-inline feebleline expand-region evil-surround evil-org evil-nerd-commenter evil-magit evil-leader evil-goggles evil-commentary evil-collection dumb-jump doom-modeline discover-my-major company-math company-c-headers adaptive-wrap ace-jump-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
